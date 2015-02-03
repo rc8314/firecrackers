b52cfd6e9251facfdae80d3a83c05385763ec380
rc8314 <rc8314@gmail.com>
2015-02-03 18:54:41 +0800

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ $(function(){
       clearTimeout(gudietime);
       $("#mask_div,#pop_div,#start_div,#home_box,#game_guide").fadeOut();
       $("#game_pane").fadeIn();
-      game.init()
+      game.init();
       boss_enter();
       hover_pane();
 
@@ -45,25 +45,7 @@ $(function(){
       play_again();
     })
   
-  
-  	//临时测试模拟触发操纵
-  	var test = 0;
-  	$("#bz").click(function(){
-  	  if (test<3){
-  	    //成功
-  	    test=test+1;
-  	    suss_game(); 
-  	  }else{
-  	    //失败
-  	    test = 0
-        $("#mask_div,#pop_div,#def_fail").show();
-  	  }	
-  	})
-  
-  
-  
-  
-  
+   
   
   
   //眨眼动画开启
@@ -124,7 +106,7 @@ function play_again(){
   $("#mask_div,#pop_div").hide();
   $("#scorebar div").html(1);
   boss_animate();
-  //
+  game.init();
 }
 
 
@@ -154,6 +136,7 @@ function hover_pane(){
 
 //成功后动画
 function suss_game(){
+  alert('2');
   var suss_boss = random(1,3);
   $("#game_boss").addClass("boss_over_"+suss_boss).delay(3000).animate({opacity:0},1200);
   $("#hover_pane").hide();
@@ -174,7 +157,7 @@ function boss_animate(){
   $("#game_sky").before("<div id='game_boss'></div>");
   $("#game_sky").after("<div id='boss_talk'></div>");
   boss_enter();
-  hover_pane();
+  //hover_pane();
 }
 
 
@@ -190,4 +173,14 @@ function guide_animate(){
 	$(".hand_off").delay(500).fadeOut(10).delay(2000).fadeIn(10);
 	$("#guide_fire").delay(500).fadeIn(10).animate({marginTop:'20%'},2000).fadeOut(10).animate({marginTop:'14%'},10);
 	$("#guide_fline").delay(500).animate({marginTop:'20%'},2020).animate({marginTop:'14%'},10);
+}
+
+
+//
+function ck_state(cc){
+  if (cc==0){
+    $("#mask_div,#pop_div,#all_fail").show();
+  }else{
+    $("#mask_div,#pop_div,#def_fail").show();
+  }
 }
